744c0e736cfd8d12753c1b2625594f3cc16231ea
Duc Hung <duchung04st@gmail.com>
2024-07-31 21:49:12 +0700

    first commit - create core user interface

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
+import { HomeComponent } from './app/home/home.component';
 import { config } from './app/app.config.server';
+import { DetailProductComponent } from './app/detail-product/detail-product.component';
+import { OrderComponent } from './app/order/order.component';
+import { OrderConfirmComponent } from './app/order-confirm/order-confirm.component';
+import { LoginComponent } from './app/login/login.component';
+import { RegisterComponent } from './app/register/register.component';
 
-const bootstrap = () => bootstrapApplication(AppComponent, config);
+const bootstrap = () => bootstrapApplication(RegisterComponent, config);
 
 export default bootstrap;
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { HomeComponent } from './app/home/home.component';
+import { DetailProductComponent } from './app/detail-product/detail-product.component';
+import { OrderComponent } from './app/order/order.component';
+import { OrderConfirmComponent } from './app/order-confirm/order-confirm.component';
+import { LoginComponent } from './app/login/login.component';
+import { RegisterComponent } from './app/register/register.component';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(RegisterComponent, appConfig)
   .catch((err) => console.error(err));
